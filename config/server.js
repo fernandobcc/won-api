981ea1dc2058f09662b3773951b71178b235d2ee
fernandobcc <fernandoxico@gmail.com>
2021-07-19 14:36:05 -0300

    feat: init strapi project and add first components

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5b15aa5c8157439c6416565f4bc632e0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2fb8559c-0b38-473f-9a32-b96534fb87f8'
+};
